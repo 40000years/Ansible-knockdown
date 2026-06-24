7b7cdd5b4506a441701ee0ba363dd6cd0fdd0e9e
40000years <thanaphatyamboo@gmail.com>
2026-06-24 11:24:27 +0700

    feat: uncomment S3 backend in providers.tf with user bucket name

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,15 +7,12 @@ terraform {
     }
   }
 
-  # -----------------------------------------------------------------------------
-  # S3 Backend: แนะนำให้เปิดใช้งานสำหรับ Semaphore เพื่อป้องกันไฟล์ state สูญหาย
-  # -----------------------------------------------------------------------------
-  # backend "s3" {
-  #   bucket         = "your-terraform-state-bucket"
-  #   key            = "ec2-fetch/terraform.tfstate"
-  #   region         = "ap-southeast-1"
-  #   dynamodb_table = "your-terraform-lock-table" # ตัวเลือกเพิ่มเติมสำหรับ State Locking
-  # }
+  backend "s3" {
+    bucket         = "thanaphat-web-app-bucket-2026-858039354188-ap-southeast-1-an"
+    key            = "ec2-fetch/terraform.tfstate"
+    region         = "ap-southeast-1"
+    # dynamodb_table = "your-terraform-lock-table" # เปิดใช้งานหากมี DynamoDB Table สำหรับ Lock State
+  }
 }
 
 provider "aws" {
